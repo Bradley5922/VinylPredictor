39ae1c7df3c10fd8295c73657818921f2a7ccc18
Bradley Cable <bradley5922@icloud.com>
2024-10-26 13:26:06 +0100

    Initial Commit

---
+++ VinylPredictor/VinylPredictorApp.swift
@@ -0,0 +1,17 @@
+//
+//  VinylPredictorApp.swift
+//  VinylPredictor
+//
+//  Created by Bradley Cable on 26/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct VinylPredictorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VinylPredictorTests/VinylPredictorTests.swift
@@ -0,0 +1,16 @@
+//
+//  VinylPredictorTests.swift
+//  VinylPredictorTests
+//
+//  Created by Bradley Cable on 26/10/2024.
+//
+
+import Testing
+
+struct VinylPredictorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
